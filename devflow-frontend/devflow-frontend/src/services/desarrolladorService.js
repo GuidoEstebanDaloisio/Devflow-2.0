4d680aafc2c7f44a4b78d352a98c157eae548d7f
Guido <guido.e.daloisio@gmail.com>
2025-08-04 23:38:09 -0300

    Se preparo para correr en 3 equipos con notas especificas

--- devflow-frontend/devflow-frontend/src/services/desarrolladorService.js
+++ devflow-frontend/devflow-frontend/src/services/desarrolladorService.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:8080/api/admin';
+
+const API_URL = `${process.env.REACT_APP_API_URL}/api/admin`;
 
 //--LISTADOS---------------------------------------------------------------------
 export const obtenerDesarrolladoresAdmin = async (filtro, estado) => {
--- devflow-frontend/devflow-frontend/src/services/loginService.js
+++ devflow-frontend/devflow-frontend/src/services/loginService.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:8080/api';
+
+const API_URL = `${process.env.REACT_APP_API_URL}/api`;
 
 export const login = async (nombreUsuario, password) => {
   const response = await axios.post(`${API_URL}/login`, {
